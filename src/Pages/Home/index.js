7a1afc0b8c95e857337af1f970bea71f34b36c1e
Jean Pierre Sissé <tavares1988@live.com>
2021-12-11 09:17:02 -0300

    Limpando e organizando o projeto por componentes e paginas, pastas header e home criados

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import Header from '../../components/Header';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Header />
+      <main>
+        Main
+      </main>
+    </div>
+  )
+}
