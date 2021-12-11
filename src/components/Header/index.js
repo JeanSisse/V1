6585e64ec16551e9fa91804b52765201402b2c07
Jean Pierre Sissé <tavares1988@live.com>
2021-12-11 18:04:15 -0300

    Estilizando as opções no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ export default function Header () {
   return (
     <div className='header display-flex-row '>
       <img src="./assets/logo-dark.svg" alt="Logo" />
-      <ul className='header__options display-flex-row font-nunito'>
+      <ul className='header__options display-flex-row font-montserrat'>
         <li className='header__options--li'>Home</li>
         <li className='header__options--li'>Portfolio</li>
         <li className='header__options--li'>Contato</li>
