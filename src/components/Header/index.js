4a40685c94ca4da4d39d18e62fbf4110cc6a838d
Jean Pierre Sissé <tavares1988@live.com>
2021-12-18 15:45:12 -0300

    Alterando background da página e estilizando um pouco mais menu opções e removendo backgroundo do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ export default function Header () {
     <div className='header display-flex-row '>
       <img src="./assets/logo-dark.svg" alt="Logo" />
       <ul className='header__options display-flex-row font-montserrat'>
-        <li className='header__options--li'>Home</li>
-        <li className='header__options--li'>Portfolio</li>
+        <li className='header__options--li contact-me'>Home</li>
+        <li className='header__options--li contact-me'>Portfolio</li>
         <li className='header__options--li contact-me'>Fala comigo</li>
       </ul>
     </div>
