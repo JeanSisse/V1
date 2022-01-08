1408d3c0445c462bc8a26aa9e984b042278a35bf
Jean Pierre Sissé <tavares1988@live.com>
2022-01-07 21:23:12 -0300

    Componente About foi criado com estrutura básica e adicionado a página

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const StyledAbaoutSection = styled.section`
   max-width: 1000px;
+  color: var(--slate);
+  font-family: var(--font-sans);
+  font-size: var(--fz-xl);
+  line-height: 1.3;
 
   .inner {
     display: grid;
@@ -14,12 +18,70 @@ const StyledAbaoutSection = styled.section`
   }
 `;
 
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+
 const About = () => {
   return(
     <StyledAbaoutSection id='about'>
-      <h2 className='numbered-heading'>Sobre</h2>
+      <h2 className='numbered-heading'>About Me</h2>
 
       <div className="inner">
+        <StyledText>
+          <>
+            <p>Hello! My name is Brittany and I enjoy creating things that live on the internet. 
+              My interest in web development started back in 2012 when I decided to try editing custom 
+              Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML &#38; CSS!
+            </p>
+            <p>Hello! My name is Brittany and I enjoy creating things that live on the internet. 
+              My interest in web development started back in 2012 when I decided to try editing custom 
+              Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML &#38; CSS!
+            </p>
+            <p>Hello! My name is Brittany and I enjoy creating things that live on the internet. 
+              My interest in web development started back in 2012 when I decided to try editing custom 
+              Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML &#38; CSS!
+            </p>
+
+            <p>
+              Here are a few technologies I&#39;ve been working with recently:
+            </p>
+          </>
+
+          <ul className="skills-list">
+            <li>JavaScript (ES6+)</li>
+            <li>React.js</li>
+            <li>Node.js</li>
+            <li>TypeScript</li>
+            <li>Express</li>
+            <li>TDD</li>
+          </ul>
+        </StyledText>
+
+        <div className="styledPic"></div>
 
       </div>
     </StyledAbaoutSection>
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -7,7 +7,7 @@ const Hero = () => {
         <h1 data-text-1='Olá, meu nome é'>Olá, meu nome é</h1>
       </div>
       <div className='h2__styled'>
-        <h2 datatext="Jean Pierre D'Almeida,">Jean Pierre D'Almeida,</h2>
+        <h2 datatext="Jean Pierre D&#39;Almeida,">Jean Pierre D'Almeida,</h2>
       </div>
       <>
         <h3>sou desenvolvedor web.</h3>
@@ -16,6 +16,14 @@ const Hero = () => {
         <h4>
           Desenvolvedor Full Stack.
         </h4>
+      </>
+      <>
+        <p>
+        I&#39;m a software engineer specializing in building 
+        (and occasionally designing) exceptional digital experiences. 
+        Currently, I&#39;m focused on building accessible, human-centered products 
+        at iFood.
+        </p>
       </>
     </section>
   );
