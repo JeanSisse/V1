6e9dc58e7e2b0d0d801784013075ff00c1d37b21
Jean Pierre Sissé <tavares1988@live.com>
2022-01-14 16:06:44 -0300

    Definindo alguns layouts e estilos comuns

---
+++ src/styles/mixs.js
@@ -0,0 +1,92 @@
+import { css } from 'styled-components';
+
+const mixs = {
+  flexCenter: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+
+  flexBetween: css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `,
+
+  flexStart: css`
+    display: flex;
+    align-items: flex-start;
+  `,
+
+  inlineLink: css`
+    display: inline-block;
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    position: relative;
+    transition: var(--transition);
+    color: var(--green);
+    &:hover,
+    &:focus,
+    &:active {
+      color: var(--green);
+      outline: 0;
+      &:after {
+        width: 100%;
+      }
+      & > * {
+        color: var(--green) !important;
+        transition: var(--transition);
+      }
+    }
+
+    &:after {
+      content: '';
+      display: block;
+      width: 0;
+      height: 1px;
+      position: relative;
+      bottom: 0.37em;
+      background-color: var(--green);
+      transition: var(--transition);
+      opacity: 0.5;
+    }
+  `,
+
+  boxShadow: css`
+    box-shadow: 0 10px 30px -15px var(--navy-shadow);
+    transition: var(--transition);
+
+    &:hover,
+    &:focus {
+      box-shadow: 0 20px 30px -15px var(--navy-shadow);
+    }
+  `,
+
+  fancyList: css`
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    font-size: var(--fz-lg);
+
+    li {
+      position: relative;
+      padding-left: 30px;
+      margin-bottom: 10px;
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+      }
+    }
+  `,
+
+  resetList: css`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  `,
+}
+
+export default mixs;
