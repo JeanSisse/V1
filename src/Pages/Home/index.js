c5f996c00967be64ee2a5fc1bd7d4fe4aac569da
Jean Pierre Sissé <tavares1988@live.com>
2022-01-14 16:08:40 -0300

    Migrando home para styled component e fazendo ajustes para responsividade da página

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,19 +1,27 @@
-import Header from '../../components/Header';
-import './styles.css';
-import Networks from '../../components/Networks';
-import Hero from '../../components/Hero';
+import styled, { ThemeProvider } from 'styled-components';
 import About from '../../components/About';
+import Header from '../../components/Header';
+import Hero from '../../components/Hero';
+import Networks from '../../components/Networks';
+import GlobalStyle from '../../styles/GlobalStyle';
+import theme from '../../styles/theme';
+import './styles.css';
+
+const StyledMainContainer = styled.main`
+  counter-reset: section;
+`;
 
 export default function Home() {
 
   return (
-    <div className=''>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Header />
-      <Networks />      
-      <main className='' id='content'>
+      <Networks />
+      <StyledMainContainer className='fillHeight' id='content'>
         <Hero />
         <About />
-      </main>
-    </div>
+      </StyledMainContainer>
+    </ThemeProvider>
   )
 }
