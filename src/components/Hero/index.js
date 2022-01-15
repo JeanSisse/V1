a35560f51c7736220aa7c959426fbd0e4d0cb6f1
Jean Pierre Sissé <tavares1988@live.com>
2022-01-15 12:24:33 -0300

    Adicionando breve descrição na Hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -19,10 +19,16 @@ const Hero = () => {
       </>
       <>
         <p>
-        I&#39;m a software engineer specializing in building 
-        (and occasionally designing) exceptional digital experiences. 
-        Currently, I&#39;m focused on building accessible, human-centered products 
-        at iFood.
+          Sou desenvolvedor de software junior especializando na Engenharia de software e 
+          apaixonado em criar experiências digitais incríveis. Atualmente estou focado na inserção do 
+          mercado de tecnologias, especificamente na área de desenvolvimento de software. 
+          Estou Ativamente buscando aperfeiçoamento dos conhecimentos adquiridos e agregando outros novos. 
+          No momento sou Engenheiro de software na <a href="https://www.ifood.com.br/">iFood</a> onde estou 
+          focado em desenvolver produtos acessíveis centrados no ser humano.
+
+          {/* I&#39;m a software engineer specializing in building exceptional digital experiences. 
+          Currently, I&#39;m focused on building accessible, human-centered products 
+          at <a href="https://www.ifood.com.br/">iFood</a>. */}
         </p>
       </>
     </StyledHeroSection>
