a63439ca8516ce01b064dce074b9d5727e444d11
Jean Pierre Sissé <tavares1988@live.com>
2022-04-05 15:52:12 -0300

    Adicionando id

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,7 +2,7 @@ import StyledHeroSection from './styles.js';
 
 const Hero = () => {
   return (
-    <StyledHeroSection className='hero__styled'>
+    <StyledHeroSection className='hero__styled' id='/'>
       <div>
         <h1>Olá, meu nome é</h1>
       </div>
@@ -19,11 +19,11 @@ const Hero = () => {
       </>
       <>
         <p>
-          Sou desenvolvedor de software junior especializando na Engenharia de software e 
-          apaixonado em criar experiências digitais incríveis. Atualmente estou focado na inserção do 
-          mercado de tecnologias, especificamente na área de desenvolvimento de software. 
-          Estou Ativamente buscando aperfeiçoamento dos conhecimentos adquiridos e agregando outros novos. 
-          No momento sou Engenheiro de software na <a href="https://www.ifood.com.br/">iFood</a> onde estou 
+          Sou desenvolvedor de software junior especializando na Engenharia de software e
+          apaixonado em criar experiências digitais incríveis. Atualmente estou focado na inserção do
+          mercado de tecnologias, especificamente na área de desenvolvimento de software.
+          Estou Ativamente buscando aperfeiçoamento dos conhecimentos adquiridos e agregando outros novos.
+          No momento sou Engenheiro de software na <a href="https://www.ifood.com.br/">iFood</a> onde estou
           focado em desenvolver produtos acessíveis centrados no ser humano.
 
           {/* I&#39;m a software engineer specializing in building exceptional digital experiences. 
