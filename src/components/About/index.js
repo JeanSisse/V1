eb1385c6644b1a920f32e8448323a8cb3c915f14
Jean Pierre Sissé <tavares1988@live.com>
2022-04-05 15:39:05 -0300

    Adicionando skills

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -120,22 +120,33 @@ const StyledPic = styled.div`
 
 const About = () => {
 
-  const skills = ['JavaScript (ES6+)', 'React', 'Node.js', 'Express'];
-  return(
-    <StyledAbaoutSection id='about'>
+  const skills = [
+    'JavaScript (ES6+)',
+    'React',
+    'Styled Component',
+    'Node.js',
+    'Express',
+    'Java 8+',
+    'Spring Boot',
+    'Spring Security',
+    'PostgresSQL'
+  ];
+
+  return (
+    <StyledAbaoutSection id='sobre'>
       <h2 className='numbered-heading'>About Me</h2>
 
       <div className="inner">
         <StyledText>
           <div>
             <p>
-              Olá, me chamo Jean Pierre - eu sou desenvolvedor full-stack que gosta de criar coisas que vivem na internet. 
-              Eu sempre tive paixão em me desafiar o que me levou a fazer curso de <b>Ciência da Computação</b> pela <a href="https://www.unb.br/">Universidade 
-              de Brasília</a> -  onde acabei 
+              Olá, me chamo Jean Pierre - eu sou desenvolvedor full-stack que gosta de criar coisas que vivem na internet.
+              Eu sempre tive paixão em me desafiar o que me levou a fazer curso de <b>Ciência da Computação</b> pela <a href="https://www.unb.br/">Universidade
+                de Brasília</a> -  onde acabei
               apaixonando pela área de tecnologia e de criação de softwares.
             </p>
             <p>
-              Também tive oportunidade de participar no curso intensivo oferecido pela <a href="https://cubos.academy/">Cubos Academy</a> em 
+              Também tive oportunidade de participar no curso intensivo oferecido pela <a href="https://cubos.academy/">Cubos Academy</a> em
               parceria com <a href="https://www.ifood.com.br/">iFood</a> onde aprendi muito sobre desenvolvimento web.
             </p>
 
