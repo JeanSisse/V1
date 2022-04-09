9b60fd84c4a9a2dd24bdeb4308e83cdfe7b3f4cc
Jean Pierre Sissé <tavares1988@live.com>
2022-04-09 14:57:58 -0300

    Adicionando formatação para link

--- src/styles/mixs.js
+++ src/styles/mixs.js
@@ -55,6 +55,21 @@ const mixs = {
     }
   `,
 
+  link: css`
+    display: inline-block;
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    color: inherit;
+    position: relative;
+    transition: var(--transition);
+    &:hover,
+    &:active,
+    &:focus {
+      color: var(--green);
+      outline: 0;
+    }
+  `,
+
   boxShadow: css`
     box-shadow: 0 10px 30px -15px var(--navy-shadow);
     transition: var(--transition);
