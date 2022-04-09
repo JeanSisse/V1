98b1a0126f2832fdaccf16d0b87150798a6856ba
Jean Pierre Sissé <tavares1988@live.com>
2022-04-09 14:58:44 -0300

    Adicionado botões de navegação entre experiências

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,9 +1,35 @@
-import StyledExperienceSection from './styles';
+import { useState } from 'react';
+import StyledExperienceSection, {
+  StyledButton,
+  StyledTabList
+} from './styles';
 
 const Experience = () => {
+  const listaDeExperiencia = ['Upstatement', 'iFood', 'Cubos', 'Pixar', 'Lexar'];
+  const [activeTabId, setActiveTabId] = useState(0);
+
   return (
-    <StyledExperienceSection>
+    <StyledExperienceSection id='experiencia'>
       <h2 className="numbered-heading">Onde trabalhei</h2>
+
+      <div className="inner">
+        <StyledTabList>
+          {
+            listaDeExperiencia &&
+            listaDeExperiencia.map(
+              (job, index) =>
+                <StyledButton
+                  id={`tab-${index}`}
+                  isActive={activeTabId === index}
+                  onClick={() => setActiveTabId(index)}
+                >
+                  <span>{job}</span>
+                </StyledButton>
+
+            )
+          }
+        </StyledTabList>
+      </div>
     </StyledExperienceSection>
   );
 }
--- src/components/Experience/styles.js
+++ src/components/Experience/styles.js
@@ -1,7 +1,62 @@
 import styled from 'styled-components';
 
 const StyledExperienceSection = styled.section`
+  max-width: 700px;
 
+  .inner {
+    display: flex;
+  }
+  
 `;
+
+export const StyledTabList = styled.div`
+  position: relative;
+  widith: max-content;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  z-index: 3;
+  
+`;
+
+export const StyledButton = styled.button`
+  ${({ theme }) => theme.mixs.link};
+  text-decoration: none;
+  text-decoration-skip-ink: auto;
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: var(--tab-height);
+  padding: 0px 20px 2px;
+  border-left: 2px solid var(--lightest-navy);
+  background-color: transparent;
+
+  color: ${({ isActive }) => (isActive ? 'var(--green)' : 'var(--slate)')};
+  font-family: var(--font-mono);
+  text-align: center;
+  white-space: nowrap;
+
+  &:hover, &:focus {
+    background-color: var(--light-navy);
+  }
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: var(--green);
+    outline: currentcolor none 0px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0 15px 2px;
+  }
+
+  @media (max-width: 600px) {
+    ${({ theme }) => theme.mixs.flexCenter};
+  }
+`;
+
 
 export default StyledExperienceSection;
